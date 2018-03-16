9e758980e258b01113f45178ca83c4c7edec6fbd
bwilm <32461222+bwilm@users.noreply.github.com>
2018-03-16 10:24:57 -0400

    added local storage

--- JustDoIt/TodoListViewController.swift
+++ JustDoIt/TodoListViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
+    let defaults = UserDefaults.standard
     var itemArray = ["Greet Pam","Bother Jim","Avoid Tobey"]
 
     override func viewDidLoad() {
+        
+        
     
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+            
+            itemArray = items
+        }
     }
-
+//MARK - Tableview datasource methods
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -34,7 +42,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
-        
+//MARK - Tableview delegate Methods
        
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
@@ -56,6 +64,8 @@ class TodoListViewController: UITableViewController {
             (action) in
             
             self.itemArray.append(textField.text! )
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
             self.tableView.reloadData()
             
